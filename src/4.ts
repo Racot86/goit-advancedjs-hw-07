5338f721de70fd4590486ee7bf4a28bffb8b48a3
dmytromayevsky <mayevskydv@gmail.com>
2024-11-01 02:19:34 +0100

    done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,57 @@
+
+class Key {
+    private signature: number = Math.random();
+
+    public getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    private key: Key;
+    constructor(key: Key) {
+        this.key = key;
+    }
+    public getKey(){
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log(`Person has entered the house.`);
+        } else {
+            console.log("The door is closed. Can't come in.");
+        }
+    }
+
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log("Door opened.");
+        } else {
+            console.log("Wrong key, door remains closed.");
+        }
+    }
+}
+
+
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
